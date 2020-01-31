77ab40146101f79c1b3bc85b64729e651acfe9ba
Ted Herring <Ted.Herring@smarsh.com>
2020-01-31 15:42:11 -0800

    aws deployment

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,12 @@ router.get('/contact', (req, res) => {
     res.render('contact/contact.ejs')
 });
 
+router.get('/tldr', (req, res) => {
+    res.render('tldr/tldr_index.ejs')
+});
+
+router.get('/future', (req, res) => {
+    res.render('future/future_index.ejs')
+});
+
 module.exports = router

7c4febc302df53cdc530aeceb2df5e2fd4ce3875
Ted Herring <Ted.Herring@smarsh.com>
2020-01-30 17:39:01 -0800

    css works now

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
-app.use(express.static('public'))
-
+app.use(express.static(__dirname + '/public'))
 app.use('/', indexRouter)
 
 app.listen(process.env.PORT || 3000)

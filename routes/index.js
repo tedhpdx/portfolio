88219ea8b2848d473a9daac73f546c341327189b
Ted Herring <Ted.Herring@smarsh.com>
2020-01-30 17:50:58 -0800

    have contact.ejs rendered now

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('index.ejs')
-})
+    res.render('index.ejs');
+});
+
+router.get('/contact', (req, res) => {
+    res.render('contact.ejs')
+});
 
 module.exports = router
